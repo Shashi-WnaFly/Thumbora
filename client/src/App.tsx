46d948af38d5b8677c90df0017b51de980e3c911
Shashi-Anand <shashianand2600@gmail.com>
2026-03-19 21:26:44 +0530

    client: adding some global css and a logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
 import "./App.css";
+import Body from "./Pages/Body";
 import HomePage from "./Pages/HomePage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <HomePage />
-    </>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;

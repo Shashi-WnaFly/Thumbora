1d31e0831ab653a1911228ef3f0e8d8ef225e38d
Shashi-Anand <shashianand2600@gmail.com>
2026-03-23 13:44:28 +0530

    client: add SectionTitle component for displaying section headers with styled text

---
+++ client/src/components/SectionTitle.tsx
@@ -0,0 +1,11 @@
+import type { ISectionTitle } from "../types";
+
+export default function SectionTitle({text1, text2, text3}: ISectionTitle) {
+  return (
+    <div className="p-2 flex justify-center items-center flex-col gap-4">
+      <p className="px-8 py-2 border border-amber-600 bg-amber-700/40 rounded-full text-orange-400">{text1}</p>
+      <h3 className="text-3xl font-semibold">{text2}</h3>
+      <p className="text-slate-300">{text3}</p>
+    </div>
+  )
+}

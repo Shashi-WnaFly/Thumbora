f15e70a1dfb4b9aa2692bce8c2ad4ddfba4d8936
Shashi-Anand <shashianand2600@gmail.com>
2026-04-12 16:52:27 +0530

    refactor: restructure routing to include ScrollToTop component for improved navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,35 +2,55 @@ import "./App.css";
 import Body from "./Pages/Body";
 import Generate from "./Pages/Generate";
 import HomePage from "./Pages/HomePage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  useLocation,
+} from "react-router-dom";
 import Login from "./Pages/Login";
 import MyGenerations from "./Pages/MyGenerations";
 import YTPreview from "./Pages/YTPreview";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return <Outlet />;
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Body />,
+    element: <ScrollToTop />,
     children: [
       {
         path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/generate/:thumbId?",
-        element: <Generate />,
-      },
-      {
-        path: "/my-generations",
-        element: <MyGenerations />,
-      },
-      {
-        path: "/preview",
-        element: <YTPreview />,
+        element: <Body />,
+        children: [
+          {
+            path: "/",
+            element: <HomePage />,
+          },
+          {
+            path: "/login",
+            element: <Login />,
+          },
+          {
+            path: "/generate/:thumbId?",
+            element: <Generate />,
+          },
+          {
+            path: "/my-generations",
+            element: <MyGenerations />,
+          },
+          {
+            path: "/preview",
+            element: <YTPreview />,
+          },
+        ],
       },
     ],
   },

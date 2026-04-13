cad7ea93da52f27e57a36e0e8018937c4ad01ca9
Shashi-Anand <shashianand2600@gmail.com>
2026-04-13 18:13:07 +0530

    server: update package.json and server.ts to include cookie-parser, cors, dotenv, and validator dependencies

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
-const PORT = process.env.PORT || 7777;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/", (req: Request, res: Response) => {
     res.send("Server is Live!");
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

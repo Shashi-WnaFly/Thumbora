0881ee3afcc7778ddd0287668f81a98f114702c5
Shashi-Anand <shashianand2600@gmail.com>
2026-04-15 14:28:36 +0530

    server: implement database connection and update dependencies

---
+++ server/config/database.ts
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET as string);
+};
+
+export default connectDB;
--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,20 @@
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 
 import authRoutes from "./routes/auth.ts";
 
 app.use("/", authRoutes);
-
-// app.use("/signup", (req: Request, res: Response) => {
-//     res.send("Server is Live!");
-// });
 
 const PORT = process.env.PORT || 5000;
 

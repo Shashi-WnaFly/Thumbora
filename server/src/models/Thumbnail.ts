fd24f107f0cc148bccf8cba50706b57f047a3d7d
Shashi-Anand <shashianand2600@gmail.com>
2026-04-19 21:20:12 +0530

    server: creating an Interface of thumbnail and mongoDb model called Thumbnail.

---
+++ server/src/models/Thumbnail.ts
@@ -0,0 +1,77 @@
+import mongoose, { Schema } from "mongoose";
+import { IThumbnail } from "../types/types.js";
+import validator from "validator";
+
+const ThumbnailSchema = new Schema<IThumbnail>(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    style: {
+      type: String,
+      enum: [
+        "Bold & Graphic",
+        "Tech/Futuristic",
+        "Minimalist",
+        "Photorealistic",
+        "Illustrated",
+      ],
+    },
+    aspectRatio: {
+      type: String,
+      enum: ["16:9", "1:1", "9:16"],
+    },
+    colorScheme: {
+      type: String,
+      enum: [
+        "vibrant",
+        "sunset",
+        "forest",
+        "neon",
+        "purple",
+        "monochrome",
+        "ocean",
+        "pastel",
+      ],
+    },
+    textOverlay: {
+      type: Boolean,
+      default: false,
+    },
+    imageUrl: {
+      type: String,
+      validate: (value: string) => {
+        if (!validator.isURL(value)) throw new Error("Invalid image url!!");
+      },
+    },
+    promptUsed: {
+      type: String,
+    },
+    userPrompt: {
+      type: String,
+      maxLength: 100,
+    },
+    isGenerating: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const Thumbnail =
+  mongoose.models.Thumbnail ||
+  mongoose.model<IThumbnail>("Thumbnail", ThumbnailSchema);
+
+export default Thumbnail;
--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 export interface IUser extends Document {
   userName: string;
   emailId: string;
@@ -22,3 +24,36 @@ export interface ISafeUser extends Document {
 }
 
 export type IGender = "male" | "female" | "other";
+
+export interface IThumbnail extends Document {
+  userId: mongoose.Schema.Types.ObjectId;
+  title: string;
+  description?: string;
+  style: IStyle;
+  aspectRatio?: IAspectRatio;
+  colorScheme?: IStyle;
+  textOverlay?: boolean;
+  imageUrl?: string;
+  promptUsed?: string;
+  userPrompt?: string;
+  isGenerating?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export type IColor =
+  | "vibrant"
+  | "sunset"
+  | "forest"
+  | "neon"
+  | "purple"
+  | "monochrome"
+  | "ocean"
+  | "pastel";
+export type IAspectRatio = "16:9" | "1:1" | "9:16";
+export type IStyle =
+  | "Bold & Graphic"
+  | "Tech/Futuristic"
+  | "Minimalist"
+  | "Photorealistic"
+  | "Illustrated";

19e287914c851c2b9dbca4bd23a50aa1e832841c
Shashi-Anand <shashianand2600@gmail.com>
2026-05-26 13:31:28 +0530

    server:feature: improve email verification logic and otp hash, early response, then save user and send email

--- server/src/routes/verify.ts
+++ server/src/routes/verify.ts
@@ -2,45 +2,64 @@ import express, { Request, Response } from "express";
 import User from "../models/User.js";
 import { PASSWORD_RESET_TEMPLATE } from "../utils/constants.js";
 import emailTransporter from "../config/emailTransporter.js";
-const crypto = await import("crypto");
+import { randomInt, createHash } from "crypto";
+import validator from "validator";
 
 const router = express.Router();
 
 router.post("/verify/reset/email", async (req: Request, res: Response) => {
   try {
+    // TODO: implement rate limiting using redis
     const { emailId } = req.body;
-    const user = await User.findOne({ emailId: emailId });
-    if (!user)
+    const normalizedEmail = emailId.trim().toLowerCase();
+
+    if (!normalizedEmail || !validator.isEmail(normalizedEmail))
       return res
         .status(400)
-        .json({ success: false, message: "Credentials are invalid!" });
-    const otp = crypto.randomInt(100001, 999999).toString();
-    user.verifyOtp = otp;
+        .json({ success: false, message: "Valid email is required!" });
+
+    const user = await User.findOne({ emailId: normalizedEmail });
+
+    res.status(200).json({
+      success: true,
+      message: "If the email exists, OTP has been sent.",
+    });
+
+    if (!user) return;
+
+    if (
+      user.otpExpireAt &&
+      user.otpExpireAt > new Date(Date.now() - 2 * 60 * 1000)
+    )
+      res.status(200).json({
+        success: true,
+        message: "",
+      });
+
+    const otp = randomInt(100000, 1000000).toString();
+    user.verifyOtp = createHash("sha256").update(otp).digest("hex"); // creating OTP hash
     user.otpExpireAt = new Date(Date.now() + 10 * 60 * 1000);
 
     const emailTemplate = PASSWORD_RESET_TEMPLATE.replace(
       "{{otp}}",
       otp,
-    ).replace("{{email}}", emailId);
+    ).replace("{{email}}", normalizedEmail);
+
     const sub = "Thumbora Password Reset OTP";
     const options = {
-      to: emailId,
+      to: normalizedEmail,
       from: process.env.SENDER_EMAIL,
       subject: sub,
       html: emailTemplate,
     };
 
-    await emailTransporter.sendMail(options);
     await user.save();
-
-    res.status(200).json({
-      success: true,
-      message: "OTP has been sent to your email for verification.",
-    });
+    await emailTransporter.sendMail(options);
   } catch (error) {
+    console.error(error);
     return res.status(500).json({
       success: false,
-      message: "An error occurred while verifying the email.",
+      message: "Internal server error",
     });
   }
 });

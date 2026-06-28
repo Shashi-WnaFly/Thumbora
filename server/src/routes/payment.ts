65a144b889e8f68181c394065101e303217ff222
Shashi-Anand <shashianand2600@gmail.com>
2026-06-28 17:33:24 +0530

    server: implement webhook handling for Razorpay payments; validate signature and update user subscription status

--- server/src/routes/payment.ts
+++ server/src/routes/payment.ts
@@ -4,6 +4,8 @@ import instance from "../config/razorpay.js";
 import { subscriptionAmount } from "../utils/constants.js";
 import { IsubscriptionType } from "../types/types.js";
 import Payment from "../models/Payment.js";
+import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils.js";
+import User from "../models/User.js";
 const router = express.Router();
 
 router.post(
@@ -53,8 +55,51 @@ router.post(
   },
 );
 
-router.post('/payment/webhook', async (req: Request, res: Response) => {
-  
-})
+router.post("/payment/webhook", async (req: Request, res: Response) => {
+  try {
+    const webhookSignature = req.get("X-Razorpay-Signature");
+
+    const isSignatureValid = validateWebhookSignature(
+      JSON.stringify(req.body),
+      webhookSignature as string,
+      process.env.RAZORPAY_WEBHOOK_SECRET as string,
+    );
+
+    if (!isSignatureValid)
+      return res
+        .status(401)
+        .json({ success: false, message: "webhook signature is invalid!" });
+
+    const paymentDetails = req.body.payload.payment.entity;
+
+    const payment = await Payment.findOne({ orderId: paymentDetails.order_id });
+
+    payment.status = paymentDetails.status;
+    payment.paymentId = paymentDetails.id;
+    await payment.save();
+
+    if (req.body.event === "payment.captured") {
+      const user = await User.findById(payment.userId);
+
+      if (!user)
+        return res
+          .status(401)
+          .json({ success: false, message: "user not found!!" });
+
+      user.subscriptionType = payment.subscriptionType;
+      user.isPremium = true;
+      await user.save();
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "webhook received successfully." });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(400)
+      .json({ success: false, message: "Interval server error." });
+  }
+});
 
 export default router;

c2705e95c97b2962e6051487908364de5336601e
Shashi-Anand <shashianand2600@gmail.com>
2026-09-14 22:05:22 +0530

    fix: update server listen address to "0.0.0.0" for external access

--- server/src/server.ts
+++ server/src/server.ts
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 5000;
 connectDB()
   .then(() => {
     console.log("database is successfully connected.");
-    app.listen(PORT, () => {
+    app.listen("0.0.0.0", () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   })

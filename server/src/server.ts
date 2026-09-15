67aff7d5e120696df2f794ddca48fad105859eac
Shashi-Anand <shashianand2600@gmail.com>
2026-09-15 22:59:06 +0530

    update CORS origin to allow requests from production URL

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://miraculous-insight-production-db4e.up.railway.app",
     credentials: true,
   }),
 );

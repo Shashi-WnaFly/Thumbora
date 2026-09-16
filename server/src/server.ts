9284e1417664bfa9c9d03086c58f57c8cda1b725
Shashi-Anand <shashianand2600@gmail.com>
2026-09-16 17:19:30 +0530

    fix: uncomment PORT declaration for server configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -34,7 +34,7 @@ app.use("/", userThumbnail);
 app.use("/", resetRoutes);
 app.use("/", paymentRoutes);
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
